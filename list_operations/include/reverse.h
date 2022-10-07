3049b703521cfbd9438086dbf80827eea7a09473
Denise Siu <denise_siu@sfu.ca>
2022-10-07 16:36:19 -0700

    reverse list working

--- list_operations/include/reverse.h
+++ list_operations/include/reverse.h
@@ -1,7 +1,15 @@
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 class Reverse
 
 {
 private:
+    vector<string> list;
+
 public:
+    vector<string> getList();
+    void setList(vector<string> newList);
+    void reverseList();
 };
--- list_operations/reverse.cpp
+++ list_operations/reverse.cpp
@@ -1,1 +1,40 @@
-#include "reverse.h"
+#include "include/reverse.h"
+#include <iostream>
+
+using namespace std;
+
+vector<string> Reverse::getList()
+{
+    return list;
+}
+
+void Reverse::setList(vector<string> newList)
+{
+    list = newList;
+}
+
+void Reverse::reverseList()
+{
+    reverse(list.begin(), list.end());
+}
+
+// int main()
+// {
+//     Reverse test;
+
+//     vector<string> ok;
+//     ok.push_back("hello");
+//     ok.push_back("one");
+//     ok.push_back("two");
+//     ok.push_back("three");
+
+//     test.setList(ok);
+//     test.reverseList();
+
+//     // printing the reversed list
+//     for (auto i : test.getList())
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+// }
