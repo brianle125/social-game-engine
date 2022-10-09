f990f39d50e1219e513b644f0633d9e1a4640889
Yiwen Wang <yiwenwang2090@cs.sfu.ca>
2022-10-09 01:10:18 -0700

    delete readJson.cpp

--- lib/game/readJson.cpp
+++ lib/game/readJson.cpp
@@ -47,8 +47,20 @@ CoinGame readJsonFile(string fileName){
     info[1].setPlayerName(player_name_2);
     g1.setConstants(info);
 
+    //rules
+    CoinRule rules[3];
+    rules[0].setRuleName(data["rules"][0]["rule"]);
+    rules[0].setlist(data["rules"][0]["List"]);
 
-    //rule
+    rules[1].setRuleName(data["rules"][1]["rule"]);
+    rules[1].setFrom(data["rules"][1]["from"]);
+    rules[1].setTo(data["rules"][1]["to"]);
+    rules[1].setCount(data["rules"][1]["count"]);
+
+    rules[2].setRuleName(data["rules"][2]["rule"]);
+    rules[2].setValue(data["rules"][2]["value"]);
+
+    g1.setRules(rules);
 
     //info.setPlayerName(data["constants"]["coin"][0]);
     //g1.setConstants(info);
@@ -74,9 +86,18 @@ int main() {
         cout<<""<<g1.getConstants()[0].getPlayerName()<<endl;
         cout<<""<<g1.getConstants()[1].getCoinName()<<endl;
         cout<<""<<g1.getConstants()[1].getPlayerName()<<endl;
-//        cout<<""<<g1.getRules()<<endl;
-//        cout<<""<<g1.getVariables()<<endl;
-//
+
+        cout<<""<<g1.getRules()[0].getRuleName()<<endl;
+        cout<<""<<g1.getRules()[0].getList()<<endl;
+
+        cout<<""<<g1.getRules()[1].getRuleName()<<endl;
+        cout<<""<<g1.getRules()[1].getFrom()<<endl;
+        cout<<""<<g1.getRules()[1].getTo()<<endl;
+        cout<<""<<g1.getRules()[1].getCount()<<endl;
+
+        cout<<""<<g1.getRules()[2].getRuleName()<<endl;
+        cout<<""<<g1.getRules()[2].getValue()<<endl;
+
         return 0;
     }
     catch (exception &e) {
