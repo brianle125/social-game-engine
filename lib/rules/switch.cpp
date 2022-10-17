6150d7e67dbdfa1a04be1eabf37fb65a00794237
mca258 <mca258@sfu.ca>
2022-10-17 05:47:15 +0000

    Update switch.cpp

--- lib/rules/switch.cpp
+++ lib/rules/switch.cpp
@@ -12,3 +12,18 @@
             }
         }
     }
+
+    //Text below was used for testing 
+    // int x = 2;
+    // std::pair<int,std::string> pair1;
+    // std::pair<int,std::string> pair2;
+    // std::pair<int,std::string> pair3;
+    // pair1.first = 1;
+    // pair1.second = "jeein1";
+    // pair2.first = 2;
+    // pair2.second = "jeein2";
+    // pair3.first = 3;
+    // pair3.second = "jeein3";
+    // std::vector<std::pair<int,std::string>> cases = {pair1,pair2,pair3};
+    // Switch mySwitch = Switch{x,cases};
+    // mySwitch.runRule();  
