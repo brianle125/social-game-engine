a38efda387bee8e1dfe395fa86ed32615fc0c1b6
mca258 <mca258@sfu.ca>
2022-10-17 05:46:51 +0000

    Update switch.h

--- lib/rules/include/switch.h
+++ lib/rules/include/switch.h
@@ -14,19 +14,4 @@ private:
     int value;
     //When we get to compilation have to change int to generic and string to rule vector
     std::vector <std::pair<int,std::string>> myVector;
-
-    //Text below was used for testing 
-    // int x = 2;
-    // std::pair<int,std::string> pair1;
-    // std::pair<int,std::string> pair2;
-    // std::pair<int,std::string> pair3;
-    // pair1.first = 1;
-    // pair1.second = "jeein1";
-    // pair2.first = 2;
-    // pair2.second = "jeein2";
-    // pair3.first = 3;
-    // pair3.second = "jeein3";
-    // std::vector<std::pair<int,std::string>> cases = {pair1,pair2,pair3};
-    // Switch mySwitch = Switch{x,cases};
-    // mySwitch.runRule();  
 };
