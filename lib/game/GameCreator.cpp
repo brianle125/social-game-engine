be0f8c341d71e187abaaaf1a6f79ffbc0298167c
Para Upton <cupton@sfu.ca>
2022-10-17 22:19:12 -0700

    refactors to separate out rule building

--- lib/game/GameCreator.cpp
+++ lib/game/GameCreator.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <memory>
-// #include <function>
 #include <nlohmann/json.hpp>
 #include "GameCreator.h"
 #include "Game.h"
@@ -42,10 +41,11 @@ gameModel::Game GameCreator::createGame() {
 }
 
 std::vector<std::unique_ptr<rules::IRule>> GameCreator::createRules(const json data) {
-	//Todo: preallocation
-	std::vector<std::unique_ptr<rules::IRule>> ruleList;
-
 	json rules = gameSource["rules"];
+
+	std::vector<std::unique_ptr<rules::IRule>> ruleList;
+	ruleList.reserve(rules.size());
+
 
 	for(auto rule : rules) {
 		auto ruleBuilder = ruleBuilders.find(rule["rule"]);
@@ -60,18 +60,3 @@ std::vector<std::unique_ptr<rules::IRule>> GameCreator::createRules(const json d
 
 
 
-void GameCreator::GenerateRuleBuilders() {
-	/*	RULE BUILDER TEMPLATE:
-	ruleBuilders.emplace("rule-name", 
-		[](json ruleData) -> std::unique_ptr<rules::IRule> {
-			//whatever you need to create your rule here
-			return std::move(newRule);
-		});
-	*/
-
-	ruleBuilders.emplace("global-message", 
-		[](json ruleData) -> std::unique_ptr<rules::IRule> {
-			auto newRule = std::make_unique<GlobalMessage>(GlobalMessage(ruleData["value"]));
-			return std::move(newRule);
-		});	
-}
