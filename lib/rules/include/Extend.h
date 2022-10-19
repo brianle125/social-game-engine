ce53bb0905c868df2dc09339af150821abce12ed
root <root@Goliath.localdomain>
2022-10-18 20:32:18 -0700

    add extend

---
+++ lib/rules/include/Extend.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include "IRule.h"
+
+class Extend{
+    public:
+        Extend(stdvector<myVariant>& originalVector,stdvector<myVariant>& additionVector);
+
+    private:
+        vector<myVariant>& originalVector
+        vector<myVariant>& additionVector
+};
