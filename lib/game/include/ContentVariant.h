618c0637768d3177c40cb2dd5b9572205873ac7c
hannah <hcr@sfu.ca>
2022-10-29 17:57:23 -0700

    Refactored variant and updated relevant cmake

--- lib/game/include/ContentVariant.h
+++ lib/game/include/ContentVariant.h
@@ -1,54 +1,100 @@
-
-#include <boost/variant.hpp>
+#pragma once
+//#include <boost/variant.hpp>
 #include <string>
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <variant>
+#include <rva/variant.hpp>
+
+#include <unordered_map>
+
+#include <iostream>
 
 using namespace std;
 
-typedef boost::make_recursive_variant<
+typedef rva::variant<
     int,
     float,
-    string, 
+    string,
     bool,
-    vector<boost::recursive_variant_>, 
-    unordered_map<string, boost::recursive_variant_>
-    >::type myVariant;
+    vector<rva::self_t>,
+    map<string, rva::self_t>
+    > myVariant;
 
-class times_two
-    : public boost::static_visitor<>
-{
-public:
+#ifndef SAMPLEVISIT_H
+#define SAMPLEVISIT_H
 
-    void operator()(int & i) const
-    {
-        i *= 2;
-    }
+struct sampleVisit {
+	void operator()(int i) {
+		cout << "int";
+	}
+	void operator()(float f) {
+		cout << "int";
+	}
+	void operator()(string s) {
+		cout << "string \n";
+	}
+	void operator()(bool b) {
+		cout << "bool \n";
+	}
+	void operator()(vector<myVariant> v) {}
+	void operator()(map<string, myVariant> m) {}
+};
 
-    void operator()(float & f) const
-    {
-        f *= 2;
-    }
-
-    void operator()(std::string & str) const
-    {
-        str += str;
-    }
-
-    void operator()(bool & b) const {}
-
-    void operator()(vector<myVariant> & v) const 
-    {
+struct doubleVisitor {
+	void operator()(int & i) {
+		i *= 2;
+	}
+	void operator()(float & f) {
+		f *= 2;
+	}
+	void operator()(string & s) {
+		s += s;
+	}
+	void operator()(bool b) {
+		//error
+	}
+	void operator()(vector<myVariant> v) {
         auto old = v.size();   
         v.reserve(2 * old);
-        copy_n(v.begin(), old, back_inserter(v));        
+        copy_n(v.begin(), old, back_inserter(v));     
     }
-
-    void operator()(unordered_map<string, myVariant> & m) const {}
-
-
+	void operator()(map<string, myVariant> m) {}
 };
+
+#endif
+
+// class times_two
+//     : public boost::static_visitor<>
+// {
+// public:
+//     void operator()(int & i) const
+//     {
+//         i *= 2;
+//     }
+
+//     void operator()(float & f) const
+//     {
+//         f *= 2;
+//     }
+
+//     void operator()(std::string & str) const
+//     {
+//         str += str;
+//     }
+
+//     void operator()(bool & b) const {}
+
+//     void operator()(vector<myVariant> & v) const 
+//     {
+//         auto old = v.size();   
+//         v.reserve(2 * old);
+//         copy_n(v.begin(), old, back_inserter(v));        
+//     }
+
+//     void operator()(unordered_map<string, myVariant> & m) const {}
+// };
 
 // class addX
 //     : public boost::static_visitor<>
