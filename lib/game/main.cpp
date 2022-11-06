57d5c898b39c9cce8c6d454fc194aafbd3cc375b
hannah <hcr@sfu.ca>
2022-11-06 14:40:15 -0800

    testing variant files

--- lib/game/main.cpp
+++ lib/game/main.cpp
@@ -1,19 +1,52 @@
 #include <GameCreator.h>
 #include <IRule.h>
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 //THIS IS A TESTING FUNCTION
 int
 main(int argc, char *argv[]) {
-	std::string filename(argv[1]);
-	GameCreator creator(filename);
-	GameController newGame = creator.createGameController();
-	newGame.printGame();
+	float f = 3.14;
+	dataVariant fl = f;
+	dataVariant i = 10;
+	dataVariant s = "hello";
+	//dataVariant f = 3.235;
+	dataVariant b = true;
 
-	// std::cout << boost::get<string>(newGame.getConstant("player-name")) << "\n";
-	// std::cout << boost::get<int>(newGame.getVariable("wins")) << "\n";
+	string out = std::visit(toStringVisitor(), i);
 
-	newGame.executeRules();
+	map<string, dataVariant> tm;
+	tm["int"] = 4;
+	tm["string"] = "plzwerk";
+
+
+	vector<dataVariant> e;
+	dataVariant em = e;
+
+	vector<dataVariant> t;
+	t.push_back(i);
+	t.push_back(s);
+	t.push_back(e);
+	dataVariant innerV = t;
+
+	vector<dataVariant> temp;
+	temp.push_back(15);
+	temp.push_back("bye");
+	temp.push_back(innerV);
+	temp.push_back(false);
+	temp.push_back(e);
+
+	dataVariant m = tm;
+	temp.push_back(m);
+
+	dataVariant v = temp;
+
+
+	cout << out << " " << std::visit(toStringVisitor(), b) << endl;
+	cout << std::visit(toStringVisitor(), v) << endl;
+	cout << std::visit(toStringVisitor(), m) << endl;
+
 	return 0;
 }
