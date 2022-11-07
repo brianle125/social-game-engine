2f1c026dcca99b04334de041e45fab9f3b4f3205
Yiwen Wang <yiwenwang2090@cs.sfu.ca>
2022-11-06 23:59:50 -0800

    missing file

---
+++ server/GameStatus.h
@@ -0,0 +1,14 @@
+//
+// Created by Yiwen Wang on 2022-11-06.
+//
+
+#ifndef SOCIAL_GAMING_GAMESTATUS_H
+#define SOCIAL_GAMING_GAMESTATUS_H
+enum class GameStatus {
+    not_started,
+    running,
+    paused,
+    terminated,
+    error
+};
+#endif //SOCIAL_GAMING_GAMESTATUS_H
